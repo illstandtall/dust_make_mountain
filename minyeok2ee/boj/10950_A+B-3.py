b880d6cd507824d2df14600a5a16a7450c742c32
illstandtall <illstandtall@gmail.com>
2022-12-15 23:46:06 +0900

    add boj 10950_A+B-3

---
+++ minyeok2ee/boj/10950_A+B-3.py
@@ -0,0 +1,25 @@
+"""
+https://www.acmicpc.net/problem/10950
+
+in
+5
+1 1
+2 3
+3 4
+9 8
+5 2
+
+out
+2
+5
+7
+17
+7
+"""
+import sys
+
+t = int(sys.stdin.readline())
+
+for i in range(t):
+    a, b = map(int, sys.stdin.readline().split())
+    print(a+b)
